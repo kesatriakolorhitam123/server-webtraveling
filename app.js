b8cd452364b05bc12c9d31a81ff60fdaa3e35f18
kesatriakolorhitam <kesatriakolorhitam123@gmail.com>
2020-11-18 22:30:08 +0700

    deploy heroku mantap

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/stisla',express.static(path.join(__dirname,'node_modules/stisla/dist')));
+app.use('/stisla',express.static(path.join(__dirname,'public/stisla/dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
